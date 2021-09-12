0d6ae88ebef1cda4519f48764033acec37bde572
MathSena <MathSena07@hotmail.com>
2021-09-12 15:30:07 -0300

    #2 - Sobre Node.js Streams - Aplicando mais testes

--- aulas/aula02/gdrive-webapi/test/_util/testeUtil.js
+++ aulas/aula02/gdrive-webapi/test/_util/testeUtil.js
@@ -1,17 +1,57 @@
-import { read } from 'fs'
-import {Readable} from 'stream'
+import {Readable, Transform, Writable} from 'stream'
+import { jest } from '@jest/globals'
 
-export default class TestUtil{
-    static generateReadableStream(data){
+export default class TestUtil {
+
+    static mockDateNow(mockImplementationPeriods) {
+        const now = jest.spyOn(global.Date, global.Date.now.name)
+    
+        mockImplementationPeriods.forEach(time => {
+          now.mockReturnValueOnce(time);
+        })
+      }
+
+    static getTimeFromDate(dateString) {
+        return new Date(dateString).getTime()
+      }
+
+    static generateReadableStream(data) {
         return new Readable({
-            objectMode: true,
-            async read(){
-                for(const item of data){
-                    this.push(item)
+          objectMode: true,
+          read() {
+            for( const item of data ) {
+              this.push(item)
+            }
+    
+            this.push(null)
+          }
+        })
+      }
+    
+      static generateWritableStream(onData) {
+        return new Writable({
+          objectMode: true,
+          write(chunk, encoding, cb) {
+            onData(chunk)
+            cb(null, chunk)
+          }
+        })
+      }
+    
+      static generateTransformStream(onData) {
+        // async function *(source) {
+        //     for await(const chunk of data) {
+        //          yield chunk   
+        //     }
+        // }
 
-                }
-                this.push(null)
+        return new Transform({
+            objectMode: true,
+            transform(chunk, enconding, cb) {
+                onData(chunk)
+                cb(null, chunk)
             }
         })
     }
 }
+
